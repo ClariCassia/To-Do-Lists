c783ecfc4f6063d1da0f4208a0844ca2cb5539e8
Clarissa de Cassia <clari.developer@gmail.com>
2024-09-03 16:17:04 -0300

    :construction: Adicionado componente NewTask

---
+++ src/components/NewTask.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input v-model="name" @keydown.enter="addTask" type="text">
+        <button @click="addTask">+</button>
+    </div>
+</template>
+
+<script>
+    export default {
+     data(){
+         return {
+            name: ""
+         }
+     }, 
+     methods: {
+         addTask(){
+             this.$emit('taskAdded', {name: this.name})
+             this.name = ""
+         }
+     }   
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
