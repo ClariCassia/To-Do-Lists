fd2e1d8098831f193951f61643f287ca044b8d68
Clarissa de Cassia <clari.developer@gmail.com>
2024-09-03 14:44:55 -0300

    :construction: Contrução componente TaskGrid

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tasks">
+  
+    <div v-for="task in tasks" :key="task.name">{{ task.name }}</div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskGrid',
+  props: {
+    tasks: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style  scoped>
+
+</style>
