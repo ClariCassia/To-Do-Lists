5f0d7f64d7423ddb5e3c9d54b4428bfeaf01a74e
Clarissa de Cassia <clari.developer@gmail.com>
2024-09-08 13:25:15 -0300

    :heavy_plus_sign: Adicionado lib fortawesome

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import './assets/base.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
+// Criando a aplicação Vue
+const app = createApp(App)
+
+// Registrando o componente do Font Awesome
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+// Montando a aplicação
+app.mount('#app')
